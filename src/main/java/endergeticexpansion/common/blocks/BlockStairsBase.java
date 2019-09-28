a8a291a0d96609d897182275523899273e2c869c
SmellyModder <smellysocks543@gmail.com>
2019-09-27 22:55:25 -0400

    Small fixes

--- src/main/java/endergeticexpansion/common/blocks/BlockStairsBase.java
+++ src/main/java/endergeticexpansion/common/blocks/BlockStairsBase.java
@@ -1,21 +1,22 @@
 package endergeticexpansion.common.blocks;
 
-import endergeticexpansion.core.registry.EEBlocks;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.StairsBlock;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 
 public class BlockStairsBase extends StairsBlock {
 
+	@SuppressWarnings("deprecation") //No, :gru_no_face:
 	public BlockStairsBase(BlockState state, Properties properties) {
 		super(state, properties);
 	}
 
 	@Override
 	public int getFlammability(BlockState state, IBlockReader world, BlockPos pos, Direction face) {
-		return state.getBlock() == EEBlocks.POISE_STAIRS ? 60 : 0;
+		return state.getBlock().isIn(BlockTags.LOGS) ? 60 : 0;
 	}
 	
 }
