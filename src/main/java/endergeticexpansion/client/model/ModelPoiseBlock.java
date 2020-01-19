d71fbdb7fc07dfdd62d3ba126c5bac24b989940c
SmellyModder <smellysocks543@gmail.com>
2020-01-19 17:51:10 -0500

    Poise Cluster Entities are now transparent

--- src/main/java/endergeticexpansion/client/model/ModelPoiseBlock.java
+++ src/main/java/endergeticexpansion/client/model/ModelPoiseBlock.java
@@ -1,5 +1,7 @@
 package endergeticexpansion.client.model;
 
+import com.mojang.blaze3d.platform.GlStateManager;
+
 import endergeticexpansion.common.entities.EntityPoiseCluster;
 import net.minecraft.client.renderer.entity.model.EntityModel;
 import net.minecraft.client.renderer.entity.model.RendererModel;
@@ -20,8 +22,13 @@ public class ModelPoiseBlock<T extends EntityPoiseCluster> extends EntityModel<T
     }
     
     @Override
-    public void render(EntityPoiseCluster entity, float f, float f1, float f2, float f3, float f4, float f5) { 
+    public void render(EntityPoiseCluster entity, float f, float f1, float f2, float f3, float f4, float f5) {
+    	GlStateManager.enableBlend();
+		GlStateManager.blendFunc(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA);
+		GlStateManager.color4f(1F, 1F, 1F, 0.3F);
         this.cube.render(f5);
+        GlStateManager.disableBlend();
+		GlStateManager.color4f(1F, 1F, 1F, 1F);
     }
     
     public void setRotateAngle(RendererModel RendererModel, float x, float y, float z) {
