2f634ffd898c3fdd3ea5d7e5fd7eb89379d45ab8
SmellyModder <smellysocks543@gmail.com>
2020-03-26 02:04:17 -0400

    Fixed Poise Cluster transparency and changed some registry names

--- src/main/java/endergeticexpansion/client/render/entity/RenderPoiseCluster.java
+++ src/main/java/endergeticexpansion/client/render/entity/RenderPoiseCluster.java
@@ -3,6 +3,7 @@ package endergeticexpansion.client.render.entity;
 import endergeticexpansion.client.model.ModelPoiseBlock;
 import endergeticexpansion.common.entities.EntityPoiseCluster;
 import endergeticexpansion.core.EndergeticExpansion;
+import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.LivingRenderer;
 import net.minecraft.util.ResourceLocation;
@@ -12,6 +13,11 @@ public class RenderPoiseCluster extends LivingRenderer<EntityPoiseCluster, Model
 	public RenderPoiseCluster(EntityRendererManager renderManager) {
         super(renderManager, new ModelPoiseBlock<>(), 0.0F);
     }
+	
+	@Override
+	protected RenderType func_230042_a_(EntityPoiseCluster cluster, boolean p_230042_2_, boolean p_230042_3_) {
+		return RenderType.getEntityTranslucent(this.getEntityTexture(cluster));
+	}
 
 	@Override
 	public ResourceLocation getEntityTexture(EntityPoiseCluster entity) {
