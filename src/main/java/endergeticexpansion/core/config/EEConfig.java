afe8497c713a3d92ce1ab96f19be3bfb7e382012
Voliant <thatvaliantvole@gmail.com>
2020-05-19 22:07:39 +0100

    Added compatibility with potato poisoning in Quark

--- src/main/java/endergeticexpansion/core/config/EEConfig.java
+++ src/main/java/endergeticexpansion/core/config/EEConfig.java
@@ -13,6 +13,9 @@ public class EEConfig {
 
 	public static class Common {
 		public final ConfigValue<Boolean> debugDragonFightManager;
+		public static ConfigValue<Boolean> poisonPotatoCompatEnabled;
+		public static ConfigValue<Boolean> poisonEffect;
+		public static ConfigValue<Double> poisonChance;
 		
 		Common(ForgeConfigSpec.Builder builder) {
 			builder.comment("Common only settings for Endergetic")
@@ -23,6 +26,22 @@ public class EEConfig {
 				.translation(makeTranslation("debug_dragon_fight_manager"))
 				.define("debugDragonFightManager", false);
 			
+			builder.pop();
+
+			builder.comment("Compatibility with Quark's poisonous potatoes feature")
+			.push("poisonousPotatoCompat");
+			poisonPotatoCompatEnabled = builder
+					.comment("If booflos can be fed a poisonous potato to stunt their growth when Quark is installed; Default: True")
+					.translation(makeTranslation("poison_potato_compat_enabled"))
+					.define("poisonPotatoCompatEnabled",true);
+			poisonEffect = builder
+					.comment("If growth stunting should give a booflo poison; Default: True")
+					.translation(makeTranslation("poison_effect"))
+					.define("poisonEffect",true);
+			poisonChance = builder
+					.comment("The chance to stunt booflo growth when feeding a poisonous potato; Default: 0.1")
+					.translation(makeTranslation("poison_chance"))
+					.defineInRange("poisonChance",0.1,0,1);
 			builder.pop();
 		}
 	}
@@ -41,13 +60,31 @@ public class EEConfig {
 	
 	public static class ValuesHolder {
 		private static boolean debugDragonFightManager;
+		public static boolean poisonPotatoCompatEnabled;
+		public static boolean poisonEffect;
+		public static double poisonChance;
 		
 		public static void updateCommonValuesFromConfig(ModConfig config) {
 			debugDragonFightManager = EEConfig.COMMON.debugDragonFightManager.get();
+			poisonPotatoCompatEnabled = EEConfig.COMMON.poisonPotatoCompatEnabled.get();
+			poisonEffect = EEConfig.COMMON.poisonEffect.get();
+			poisonChance = EEConfig.COMMON.poisonChance.get();
 		}
 		
 		public static boolean shouldDebugDragonFightManager() {
 			return debugDragonFightManager;
+		}
+
+		public static boolean shouldEnablePoisonPotatoCompat() {
+			return poisonPotatoCompatEnabled;
+		}
+
+		public static boolean shouldPoisonEntity() {
+			return poisonEffect;
+		}
+
+		public static double poisonEffectChance() {
+			return poisonChance;
 		}
 	}
 	
--- src/main/java/endergeticexpansion/core/events/CompatEvents.java
+++ src/main/java/endergeticexpansion/core/events/CompatEvents.java
@@ -1,22 +1,38 @@
 package endergeticexpansion.core.events;
 
+import com.teamabnormals.abnormals_core.common.network.particle.MessageC2S2CSpawnParticle;
+import com.teamabnormals.abnormals_core.core.library.endimator.entity.EndimatedEntity;
 import endergeticexpansion.common.entities.booflo.EntityBooflo;
 import endergeticexpansion.common.entities.booflo.EntityBoofloAdolescent;
 import endergeticexpansion.common.entities.booflo.EntityBoofloBaby;
 import endergeticexpansion.core.EndergeticExpansion;
+import endergeticexpansion.core.config.EEConfig;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Items;
+import net.minecraft.particles.ParticleTypes;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.potion.Effects;
 import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.SoundEvents;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.server.ServerWorld;
+import net.minecraftforge.event.entity.living.LivingEvent;
 import net.minecraftforge.event.entity.living.PotionEvent;
+import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
 
 /**
  * @author Voliant
- * Events for compatibility with the Savage & Ravage mod.
+ * Events for compatibility with other mods.
  */
 @Mod.EventBusSubscriber(modid = EndergeticExpansion.MOD_ID)
 public class CompatEvents {
+
+    //Savage & Ravage potions of growing/youth
     @SubscribeEvent
     public static void onPotionExpire(PotionEvent.PotionExpiryEvent event) {
         LivingEntity affected = event.getEntityLiving();
@@ -34,5 +50,48 @@ public class CompatEvents {
             if(isBabyEffect && affected instanceof EntityBooflo) ((EntityBooflo)affected).growDown();
         }
 
+    }
+
+    //Quark potato poisoning
+    public static String poisonTag = "endergetic:poisoned_by_potato";
+
+    @SubscribeEvent
+    public static void onInteractWithEntity(PlayerInteractEvent.EntityInteract event){
+        Entity target = event.getTarget();
+        if((target instanceof EntityBoofloBaby || target instanceof EntityBoofloAdolescent) && event.getItemStack().getItem() == Items.POISONOUS_POTATO && EEConfig.ValuesHolder.shouldEnablePoisonPotatoCompat() && ModList.get().isLoaded("quark")) {
+            if(!target.getPersistentData().getBoolean(poisonTag) && !event.getWorld().isRemote) {
+                //Vec3d pos = target.getPositionVec();
+                event.getPlayer().swingArm(event.getHand());
+                if(target.world.rand.nextDouble() < EEConfig.ValuesHolder.poisonEffectChance()) {
+                    target.playSound(SoundEvents.ENTITY_GENERIC_EAT, 0.5f, 0.25f);
+                    //TODO reactivate this if fixed in Quark
+                    //if(((LivingEntity)target).isServerWorld()) ((ServerWorld)target.world).spawnParticle(ParticleTypes.ENTITY_EFFECT, pos.x, pos.y, pos.z, 5, 0, 1.0, 0, 0.8);
+                    target.getPersistentData().putBoolean(poisonTag, true);
+                    if(EEConfig.ValuesHolder.shouldPoisonEntity()) {
+                        ((LivingEntity) target).addPotionEffect(new EffectInstance(Effects.POISON, 200));
+                    } else {
+                        target.playSound(SoundEvents.ENTITY_GENERIC_EAT, 0.5f, 0.5f + target.world.rand.nextFloat() / 2);
+                        //if(((LivingEntity)target).isServerWorld()) ((ServerWorld)target.world).spawnParticle(ParticleTypes.SMOKE, pos.x, pos.y, pos.z, 5, 0, 1.0, 0, 0.1);
+                    }
+                    if(!event.getPlayer().isCreative()) event.getItemStack().shrink(1);
+                }
+            }
+
+        }
+
+    }
+
+    @SubscribeEvent
+    public static void onEntityUpdate(LivingEvent.LivingUpdateEvent event) {
+        boolean isBabyBooflo = event.getEntityLiving() instanceof EntityBoofloBaby;
+        if((isBabyBooflo || event. getEntityLiving() instanceof EntityBoofloAdolescent) && EEConfig.ValuesHolder.shouldEnablePoisonPotatoCompat() && ModList.get().isLoaded("quark")) {
+            if(event.getEntityLiving().getPersistentData().getBoolean(poisonTag)){
+                if(isBabyBooflo) {
+                    ((EntityBoofloBaby) event.getEntityLiving()).setGrowingAge(-24000);
+                } else {
+                    ((EntityBoofloAdolescent) event.getEntityLiving()).setGrowingAge(-24000);
+                }
+            }
+        }
     }
 }
