aaaac9f228dd8440c2c8f2dd8f320732961aa4ad
SmellyModder <smellysocks543@gmail.com>
2020-11-04 08:58:51 -0500

    Closes #118

--- src/main/java/com/minecraftabnormals/endergetic/client/renderers/entity/layer/LayerRendererPuffBugGlow.java
+++ src/main/java/com/minecraftabnormals/endergetic/client/renderers/entity/layer/LayerRendererPuffBugGlow.java
@@ -1,12 +1,9 @@
 package com.minecraftabnormals.endergetic.client.renderers.entity.layer;
 
-import com.mojang.blaze3d.matrix.MatrixStack;
-import com.mojang.blaze3d.vertex.IVertexBuilder;
-import com.teamabnormals.abnormals_core.client.ACRenderTypes;
-import com.teamabnormals.abnormals_core.client.ClientInfo;
 import com.minecraftabnormals.endergetic.common.entities.puffbug.PuffBugEntity;
 import com.minecraftabnormals.endergetic.core.EndergeticExpansion;
-
+import com.mojang.blaze3d.matrix.MatrixStack;
+import com.teamabnormals.abnormals_core.client.ACRenderTypes;
 import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.entity.IEntityRenderer;
 import net.minecraft.client.renderer.entity.layers.LayerRenderer;
@@ -18,11 +15,9 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 
 @OnlyIn(Dist.CLIENT)
 public class LayerRendererPuffBugGlow<T extends PuffBugEntity, M extends EntityModel<T>> extends LayerRenderer<T, M> {
-	private static final ResourceLocation[] TEXTURES = new ResourceLocation[]{
-			new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/puffbug/puffbug_inflated_overlay.png"),
-			new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/puffbug/puffbug_inflated_overlay_grayscale.png"),
-			new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/puffbug/puffbug_inflated_levitation_overlay.png"),
-	};
+	private static final ResourceLocation GRAYSCALE_TEXTURE = new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/puffbug/puffbug_inflated_overlay_grayscale.png");
+	private static final ResourceLocation POLLINATED_TEXTURE = new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/puffbug/puffbug_inflated_levitation_overlay.png");
+	private static final ResourceLocation DEFAULT_TEXTURE = new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/puffbug/puffbug_inflated_overlay.png");
 
 	public LayerRendererPuffBugGlow(IEntityRenderer<T, M> entityRenderer) {
 		super(entityRenderer);
@@ -32,32 +27,19 @@ public class LayerRendererPuffBugGlow<T extends PuffBugEntity, M extends EntityM
 	public void render(MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn, T puffbug, float limbSwing, float limbSwingAmount, float partialTicks, float ageInTicks, float netHeadYaw, float headPitch) {
 		if (!puffbug.isInflated()) return;
 
-		boolean colorFlag = puffbug.getColor() != -1 && !isLeviationOnlyEffect(puffbug);
-
-		float[] rgb = new float[]{
-				colorFlag ? (float) ((puffbug.getColor() >> 16 & 255) / 255.0D) : 1.0F,
-				colorFlag ? (float) ((puffbug.getColor() >> 8 & 255) / 255.0D) : 1.0F,
-				colorFlag ? (float) ((puffbug.getColor() & 255) / 255.0D) : 1.0F,
-		};
-
-		ClientInfo.MINECRAFT.getTextureManager().bindTexture(this.getTexture(puffbug));
-		IVertexBuilder ivertexbuilder = bufferIn.getBuffer(ACRenderTypes.getEmissiveEntity(this.getTexture(puffbug)));
-
-		this.getEntityModel().setRotationAngles(puffbug, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
-		this.getEntityModel().render(matrixStackIn, ivertexbuilder, 240, OverlayTexture.NO_OVERLAY, rgb[0], rgb[1], rgb[2], 1.0F);
-	}
-
-	private ResourceLocation getTexture(PuffBugEntity puffbug) {
-		if (this.isLeviationOnlyEffect(puffbug)) {
-			return TEXTURES[2];
+		M model = this.getEntityModel();
+		model.setRotationAngles(puffbug, limbSwing, limbSwingAmount, ageInTicks, netHeadYaw, headPitch);
+		int color = puffbug.getColor();
+		switch (color) {
+			case -1:
+				model.render(matrixStackIn, bufferIn.getBuffer(ACRenderTypes.getEmissiveEntity(DEFAULT_TEXTURE)), 240, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+				break;
+			case 13565951:
+				model.render(matrixStackIn, bufferIn.getBuffer(ACRenderTypes.getEmissiveEntity(POLLINATED_TEXTURE)), 240, OverlayTexture.NO_OVERLAY, 1.0F, 1.0F, 1.0F, 1.0F);
+				break;
+			default:
+				model.render(matrixStackIn, bufferIn.getBuffer(ACRenderTypes.getEmissiveEntity(GRAYSCALE_TEXTURE)), 240, OverlayTexture.NO_OVERLAY, (color >> 16 & 255) / 255.0F, (color >> 8 & 255) / 255.0F, (color & 255) / 255.0F, 1.0F);
+				break;
 		}
-		return puffbug.getColor() != -1 ? TEXTURES[1] : TEXTURES[0];
-	}
-
-	private boolean isLeviationOnlyEffect(PuffBugEntity bug) {
-		if (bug.getColor() == 13565951) {
-			return true;
-		}
-		return false;
 	}
 }
