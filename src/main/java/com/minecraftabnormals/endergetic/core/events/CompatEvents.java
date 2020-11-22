4d30b0c182ffe814d0de0915424ac9095ec752d0
abigailfails <thatvaliantvole@gmail.com>
2020-11-22 10:30:00 +0000

    Reduced redundancy

--- src/main/java/com/minecraftabnormals/endergetic/core/events/CompatEvents.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/events/CompatEvents.java
@@ -6,11 +6,14 @@ import com.minecraftabnormals.endergetic.common.entities.booflo.BoofloEntity;
 import com.minecraftabnormals.endergetic.core.EndergeticExpansion;
 
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.potion.Effect;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Objects;
 
 /**
  * @author abigailfails
@@ -18,11 +21,16 @@ import net.minecraftforge.registries.ForgeRegistries;
  */
 @Mod.EventBusSubscriber(modid = EndergeticExpansion.MOD_ID)
 public final class CompatEvents {
+
+	private static final ResourceLocation SHRINKING_ID = new ResourceLocation("savageandravage:shrinking");
+	private static final ResourceLocation GROWING_ID = new ResourceLocation("savageandravage:growing");
+
 	@SubscribeEvent
 	public static void onPotionExpire(PotionEvent.PotionExpiryEvent event) {
 		LivingEntity affected = event.getEntityLiving();
-		boolean isBabyEffect = event.getPotionEffect().getPotion() == ForgeRegistries.POTIONS.getValue(new ResourceLocation("savageandravage:shrinking"));
-		if (isBabyEffect || event.getPotionEffect().getPotion() == ForgeRegistries.POTIONS.getValue(new ResourceLocation("savageandravage:growing"))) {
+		Effect effectType = Objects.requireNonNull(event.getPotionEffect()).getPotion();
+		boolean isBabyEffect = effectType == ForgeRegistries.POTIONS.getValue(SHRINKING_ID);
+		if (isBabyEffect || effectType == ForgeRegistries.POTIONS.getValue(GROWING_ID)) {
 			if (!isBabyEffect && affected instanceof BoofloBabyEntity) ((BoofloBabyEntity) affected).growUp();
 			if (affected instanceof BoofloAdolescentEntity) {
 				if (isBabyEffect) {
