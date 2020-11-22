4263a68f605225f0b9fa43e986eef659df57fd63
abigailfails <thatvaliantvole@gmail.com>
2020-11-22 18:25:38 +0000

    Added null check and removed blank line

--- src/main/java/com/minecraftabnormals/endergetic/core/events/CompatEvents.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/events/CompatEvents.java
@@ -7,13 +7,12 @@ import com.minecraftabnormals.endergetic.core.EndergeticExpansion;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.entity.living.PotionEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.ForgeRegistries;
-
-import java.util.Objects;
 
 /**
  * @author abigailfails
@@ -21,25 +20,27 @@ import java.util.Objects;
  */
 @Mod.EventBusSubscriber(modid = EndergeticExpansion.MOD_ID)
 public final class CompatEvents {
-
 	private static final ResourceLocation SHRINKING_ID = new ResourceLocation("savageandravage:shrinking");
 	private static final ResourceLocation GROWING_ID = new ResourceLocation("savageandravage:growing");
 
 	@SubscribeEvent
 	public static void onPotionExpire(PotionEvent.PotionExpiryEvent event) {
 		LivingEntity affected = event.getEntityLiving();
-		Effect effectType = Objects.requireNonNull(event.getPotionEffect()).getPotion();
-		boolean isBabyEffect = effectType == ForgeRegistries.POTIONS.getValue(SHRINKING_ID);
-		if (isBabyEffect || effectType == ForgeRegistries.POTIONS.getValue(GROWING_ID)) {
-			if (!isBabyEffect && affected instanceof BoofloBabyEntity) ((BoofloBabyEntity) affected).growUp();
-			if (affected instanceof BoofloAdolescentEntity) {
-				if (isBabyEffect) {
-					((BoofloAdolescentEntity) affected).growDown();
-				} else {
-					((BoofloAdolescentEntity) affected).growUp();
+		EffectInstance effectInstance = event.getPotionEffect();
+		if(effectInstance != null) {
+			Effect effectType = effectInstance.getPotion();
+			boolean isBabyEffect = effectType == ForgeRegistries.POTIONS.getValue(SHRINKING_ID);
+			if (isBabyEffect || effectType == ForgeRegistries.POTIONS.getValue(GROWING_ID)) {
+				if (!isBabyEffect && affected instanceof BoofloBabyEntity) ((BoofloBabyEntity) affected).growUp();
+				if (affected instanceof BoofloAdolescentEntity) {
+					if (isBabyEffect) {
+						((BoofloAdolescentEntity) affected).growDown();
+					} else {
+						((BoofloAdolescentEntity) affected).growUp();
+					}
 				}
+				if (isBabyEffect && affected instanceof BoofloEntity) ((BoofloEntity) affected).growDown();
 			}
-			if (isBabyEffect && affected instanceof BoofloEntity) ((BoofloEntity) affected).growDown();
 		}
 	}
 }
