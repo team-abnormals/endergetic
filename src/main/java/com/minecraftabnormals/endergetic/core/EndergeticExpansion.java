560a259989d8ec71241606d656c38db2929943fb
SmellyModder <smellysocks543@gmail.com>
2020-12-06 22:18:02 -0500

    Delete leftover PoiseForestBiome

--- src/main/java/com/minecraftabnormals/endergetic/core/EndergeticExpansion.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/EndergeticExpansion.java
@@ -69,6 +69,7 @@ public class EndergeticExpansion {
 			.serverAcceptedVersions(NETWORK_PROTOCOL::equals)
 			.simpleChannel();
 
+	@SuppressWarnings("deprecation")
 	public EndergeticExpansion() {
 		instance = this;
 
