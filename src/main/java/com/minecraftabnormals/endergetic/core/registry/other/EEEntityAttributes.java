f770281d7cfa88155b7d6e5dd77dfcf29b34ce0a
SmellyModder <smellysocks543@gmail.com>
2021-01-03 16:49:12 -0500

    Slightly nerfed Eetle movement speed

--- src/main/java/com/minecraftabnormals/endergetic/core/registry/other/EEEntityAttributes.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/registry/other/EEEntityAttributes.java
@@ -50,7 +50,7 @@ public final class EEEntityAttributes {
 				MobEntity.func_233666_p_()
 						.createMutableAttribute(Attributes.ATTACK_DAMAGE, 7.0F)
 						.createMutableAttribute(Attributes.ATTACK_KNOCKBACK, 1.0F)
-						.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.225F)
+						.createMutableAttribute(Attributes.MOVEMENT_SPEED, 0.2F)
 						.createMutableAttribute(Attributes.ARMOR, 4.0F)
 						.createMutableAttribute(Attributes.MAX_HEALTH, 30.0F)
 						.createMutableAttribute(Attributes.FOLLOW_RANGE, 32.0F)
