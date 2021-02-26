d52360b4b04b5ad90b36927c9272a644f9e8c4ca
bageldotjpg <55970498+bageldotjpg@users.noreply.github.com>
2021-02-26 13:28:37 -0600

    Fix Poise Post

--- src/main/java/com/minecraftabnormals/endergetic/core/registry/EEBlocks.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/registry/EEBlocks.java
@@ -92,7 +92,7 @@ public final class EEBlocks {
 	public static final RegistryObject<Block> POISE_BEEHIVE         = HELPER.createCompatBlock("buzzier_bees", "poise_beehive", () -> new AbnormalsBeehiveBlock(Properties.from(Blocks.BEEHIVE)), ItemGroup.DECORATIONS);
 	public static final RegistryObject<Block> STRIPPED_POISE_POST 	= HELPER.createCompatFuelBlock("quark", "stripped_poise_post", () -> new WoodPostBlock(EEProperties.POISE_WOOD), 300, ItemGroup.BUILDING_BLOCKS);
 	public static final RegistryObject<Block> POISE_POST 			= HELPER.createCompatFuelBlock("quark", "poise_post", () -> new WoodPostBlock(STRIPPED_POISE_POST, EEProperties.POISE_WOOD), 300, ItemGroup.BUILDING_BLOCKS);
-	public static final RegistryObject<Block> GLOWING_POISE_POST 	= HELPER.createCompatFuelBlock("quark", "glowing_poise_post", () -> new WoodPostBlock(EEProperties.POISE_LOG_GLOWING), 300, ItemGroup.BUILDING_BLOCKS);
+	public static final RegistryObject<Block> GLOWING_POISE_POST 	= HELPER.createCompatFuelBlock("quark", "glowing_poise_post", () -> new WoodPostBlock(STRIPPED_POISE_POST, EEProperties.POISE_LOG_GLOWING), 300, ItemGroup.BUILDING_BLOCKS);
 	public static final RegistryObject<Block> POISE_HEDGE 			= HELPER.createCompatFuelBlock("quark", "poise_hedge", () -> new HedgeBlock(EEProperties.POISE_WOOD), 300, ItemGroup.DECORATIONS);
 	public static final Pair<RegistryObject<AbnormalsStandingSignBlock>, RegistryObject<AbnormalsWallSignBlock>> POISE_SIGN  = HELPER.createSignBlock("poise", MaterialColor.PURPLE_TERRACOTTA);
 	public static final Pair<RegistryObject<AbnormalsChestBlock>, RegistryObject<AbnormalsTrappedChestBlock>> 	 POISE_CHEST = HELPER.createCompatChestBlocks("poise", MaterialColor.PURPLE_TERRACOTTA);
