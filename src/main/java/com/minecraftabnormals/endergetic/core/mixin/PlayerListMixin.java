53894c08abde8c84096acc8123b2cf631af06bcd
Luke Tonon <41268323+SmellyModder@users.noreply.github.com>
2021-03-23 13:55:26 -0400

    Removed unused import

--- src/main/java/com/minecraftabnormals/endergetic/core/mixin/PlayerListMixin.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/mixin/PlayerListMixin.java
@@ -13,7 +13,6 @@ import net.minecraft.nbt.ListNBT;
 import net.minecraft.network.NetworkManager;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
-import net.minecraft.util.registry.DynamicRegistries;
 import net.minecraft.world.server.ServerWorld;
 import net.minecraft.world.storage.PlayerData;
 import net.minecraftforge.fml.network.PacketDistributor;
