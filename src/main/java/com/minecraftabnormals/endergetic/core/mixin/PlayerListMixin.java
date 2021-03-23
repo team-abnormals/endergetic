7aab8b327c17e0d4f96b7a32a45d2a37b8c32577
Bob Dole <8822548+wchen1990@users.noreply.github.com>
2021-03-23 14:06:03 -0400

    Change spawnBalloons mixin implementation (#163)
    * Change spawnBalloons mixin implementation
    
    - Avoids calling a Player Entity's read method from being called
      multiple times when it's not necessary
    - Quick fix for #162
    
    * Address code review suggestions
    
    - Use @Shadow and @Final
    - Removed constructor mixin
    - Changed file1 to playerDataFile
    - Changed var4 to exception
    
    * Conform to code base styling
    
    * Removed unused import
    
    Co-authored-by: Luke Tonon <41268323+SmellyModder@users.noreply.github.com>

--- src/main/java/com/minecraftabnormals/endergetic/core/mixin/PlayerListMixin.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/mixin/PlayerListMixin.java
@@ -8,20 +8,32 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.CompressedStreamTools;
 import net.minecraft.nbt.ListNBT;
 import net.minecraft.network.NetworkManager;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.management.PlayerList;
 import net.minecraft.world.server.ServerWorld;
+import net.minecraft.world.storage.PlayerData;
 import net.minecraftforge.fml.network.PacketDistributor;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+import java.io.File;
 import java.util.List;
 
 @Mixin(PlayerList.class)
 public final class PlayerListMixin {
+	@Shadow
+	@Final
+	private PlayerData playerDataManager;
+	@Shadow
+	@Final
+	private MinecraftServer server;
 
 	@SuppressWarnings("deprecation")
 	@Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/server/management/PlayerList;writePlayerData(Lnet/minecraft/entity/player/ServerPlayerEntity;)V", shift = At.Shift.AFTER), method = "playerLoggedOut")
@@ -40,7 +52,19 @@ public final class PlayerListMixin {
 	@Inject(at = @At("RETURN"), method = "initializeConnectionToPlayer")
 	private void spawnBalloons(NetworkManager netManager, ServerPlayerEntity player, CallbackInfo info) {
 		ServerWorld serverWorld = (ServerWorld) player.world;
-		CompoundNBT compound = ((PlayerList) (Object) this).readPlayerDataFromFile(player);
+
+		CompoundNBT compound = this.server.getServerConfiguration().getHostPlayerNBT();
+		if (!(compound != null && player.getName().getString().equals(this.server.getServerOwner()))) {
+			try {
+				File playerDataFile = new File(this.playerDataManager.getPlayerDataFolder(), player.getCachedUniqueIdString() + ".dat");
+				if (playerDataFile.exists() && playerDataFile.isFile()) {
+					compound = CompressedStreamTools.readCompressed(playerDataFile);
+				}
+			} catch (Exception exception) {
+				EndergeticExpansion.LOGGER.warn("Failed to load player data for {}", player.getName().getString());
+			}
+		}
+
 		if (compound != null && compound.contains("Balloons", 9)) {
 			ListNBT balloonsTag = compound.getList("Balloons", 10);
 			if (!balloonsTag.isEmpty()) {
