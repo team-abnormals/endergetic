c75c00f26fb92f019d7e43c73a64bc113333038a
SmellyModder <smellysocks543@gmail.com>
2021-07-06 15:55:45 -0400

    Adjusted Purpoid shadow size

--- src/main/java/com/minecraftabnormals/endergetic/client/renderers/entity/PurpoidRenderer.java
+++ src/main/java/com/minecraftabnormals/endergetic/client/renderers/entity/PurpoidRenderer.java
@@ -7,6 +7,7 @@ import com.minecraftabnormals.endergetic.client.renderers.entity.layer.PurpoidGe
 import com.minecraftabnormals.endergetic.common.entities.purpoid.PurpoidEntity;
 import com.minecraftabnormals.endergetic.core.EndergeticExpansion;
 import com.mojang.blaze3d.matrix.MatrixStack;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
@@ -22,9 +23,15 @@ public class PurpoidRenderer extends MobRenderer<PurpoidEntity, PurpoidModel> {
 	public static final ResourceLocation EMISSIVE_TEXTURE = new ResourceLocation(EndergeticExpansion.MOD_ID, "textures/entity/purpoid/purpoid_emissive.png");
 
 	public PurpoidRenderer(EntityRendererManager renderManagerIn) {
-		super(renderManagerIn, new PurpoidModel(), 0.5F);
+		super(renderManagerIn, new PurpoidModel(), 0.6F);
 		this.addLayer(new EmissiveLayerRenderer<>(this, EMISSIVE_TEXTURE));
 		this.addLayer(new PurpoidGelLayer(this));
+	}
+
+	@Override
+	public void render(PurpoidEntity purpoid, float entityYaw, float partialTicks, MatrixStack matrixStackIn, IRenderTypeBuffer bufferIn, int packedLightIn) {
+		this.shadowSize = 0.6F * purpoid.getSize().getScale();
+		super.render(purpoid, entityYaw, partialTicks, matrixStackIn, bufferIn, packedLightIn);
 	}
 
 	@Override
