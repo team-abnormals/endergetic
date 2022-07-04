3c7ac8ea8c5b40bc33b5e52fbeb08e2897a81eb6
SmellyModder <smellysocks543@gmail.com>
2022-07-04 17:34:33 -0400

    Eetle Nest biome tag datagen

--- src/main/java/com/minecraftabnormals/endergetic/core/data/server/tags/EEBiomeTagsProvider.java
+++ src/main/java/com/minecraftabnormals/endergetic/core/data/server/tags/EEBiomeTagsProvider.java
@@ -2,9 +2,11 @@ package com.minecraftabnormals.endergetic.core.data.server.tags;
 
 import com.minecraftabnormals.endergetic.core.EndergeticExpansion;
 import com.minecraftabnormals.endergetic.core.registry.EEBiomes;
+import com.minecraftabnormals.endergetic.core.registry.other.EETags;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.tags.BiomeTagsProvider;
 import net.minecraft.tags.BiomeTags;
+import net.minecraft.world.level.biome.Biomes;
 import net.minecraftforge.common.Tags;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -24,6 +26,7 @@ public final class EEBiomeTagsProvider extends BiomeTagsProvider {
 		this.tag(Tags.Biomes.IS_SPARSE_END).add(EEBiomes.POISE_FOREST.get());
 		this.tag(Tags.Biomes.IS_MAGICAL).add(EEBiomes.POISE_FOREST.get());
 		this.tag(Tags.Biomes.IS_END).add(EEBiomes.POISE_FOREST.get());
+		this.tag(EETags.Biomes.HAS_EETLE_NEST).add(Biomes.END_HIGHLANDS);
 	}
 
 }
